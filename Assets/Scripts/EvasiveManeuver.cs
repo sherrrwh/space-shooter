ef013ca9925a34dd89eff7b1133f88228d721528
nora <sherlockwh@gmail.com>
2018-05-23 18:05:09 +0800

    FirstCommit

---
+++ Assets/Scripts/EvasiveManeuver.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 控制Enemy在X轴移动
+public class EvasiveManeuver : MonoBehaviour {
+
+	public float dodge;
+	public float smoothing;
+	public float tilt;
+	public Vector2 startWait;
+	public Vector2 maneuverTime;
+	public Vector2 maneuverWait;
+	public Boundary boundary;
+
+	private float currentSpeed;
+	private float targetManeuver;
+	private Rigidbody rb;
+
+	void Start() {
+		rb = GetComponent<Rigidbody>();
+		// 记录一开始的z轴速度
+		currentSpeed = rb.velocity.z;
+		// StartCoroutine是执行一个需要等待的函数
+		StartCoroutine(Evade());
+	}
+	// 需要等待的函数就用IEnumerator
+	IEnumerator Evade() {
+		// xxx return是等待
+		// 随机等待startWait.x - startWait.y秒
+		yield return new WaitForSeconds(Random.Range(startWait.x, startWait.y));
+
+		while (true) {
+			targetManeuver = Random.Range(1, dodge) * -Mathf.Sign(GetComponent<Transform>().position.x);
+			yield return new WaitForSeconds(Random.Range(maneuverTime.x, maneuverTime.y));
+			targetManeuver = 0;
+			yield return new WaitForSeconds(Random.Range(maneuverWait.x, maneuverWait.y));
+		}
+	}
+	// 每0.02s执行一次
+	void FixedUpdate() {
+		// 游戏里有两种更新频率，一种是每帧更新，一种是个固定时长更新
+		// 物理引擎是固定时长更新（2个原因）
+		float newManeuver = Mathf.MoveTowards(rb.velocity.x, targetManeuver, Time.deltaTime * smoothing);
+		rb.velocity = new Vector3(newManeuver, 0.0f, currentSpeed);
+		// 限制Enemy不要飞出场景
+		rb.position = new Vector3
+		(
+			Mathf.Clamp(rb.position.x, boundary.xmin, boundary.xmax),
+			0.0f,
+			Mathf.Clamp(rb.position.z, boundary.zmin, boundary.zmax)
+		);
+		//讲一个欧拉角转换成四元数
+		rb.rotation = Quaternion.Euler(0.0f, 0.0f, rb.velocity.x * -tilt); // 倾斜角度
+	}
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour {
+
+	public GameObject[] hazards;
+	public Vector3 spawnValues;
+	public int hazardCount;
+	public float spawnWait;
+	public float startWait;
+	public float waveWait;
+
+	public Text scoreText;
+	public Text restartText;
+	public Text gameOverText;
+
+	private int score;
+	private bool restart;
+	private bool gameOver;
+
+	void Start() {
+		gameOver = false;
+		restart = false;
+		restartText.text = "";
+		gameOverText.text = "";
+		score = 0;
+		UpdateScore();
+		StartCoroutine(SpawnWaves());
+	}
+
+	void Update() {
+		if (restart) {
+			if (Input.GetKeyDown(KeyCode.R)) {
+				Application.LoadLevel(Application.loadedLevel);
+				//SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex); // 新寫法
+			}
+		}
+	}
+
+	IEnumerator SpawnWaves() {
+		yield return new WaitForSeconds(startWait);
+		while (true) {
+			for (int i = 0; i < hazardCount; i++) {
+				GameObject hazard = hazards[Random.Range(0, hazards.Length)];
+				/*bool flag = (Random.value > 0.5f)
+				  if (flag) {
+					  spawnValues.z (16 or -16)
+				  } */
+				Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+				Quaternion spawnRotation = Quaternion.identity;
+				GameObject clone = Instantiate(hazard, spawnPosition, spawnRotation);//创建一个障碍物，赋值给clone
+				ReverseDirection (clone);
+				yield return new WaitForSeconds(spawnWait);
+			}
+			yield return new WaitForSeconds(waveWait);
+			if (gameOver) {
+				restartText.text = "Press 'R' for Restart";
+				restart = true;
+				break;
+			}
+		}
+	}
+//保证一个石头面向上方，飞向下方
+	void ReverseDirection (GameObject clone) {
+		Quaternion r = clone.transform.rotation;
+		r.y = 0;
+		clone.transform.rotation = r;
+		clone.GetComponent<Mover>().speed = -5;
+	}
+
+	public void AddScore(int newScoreValue) {
+		score += newScoreValue;
+		UpdateScore();
+	}
+
+	void UpdateScore() {
+		scoreText.text = "Score: " + score;
+	}
+
+	public void GameOver() {
+		gameOverText.text = "Game Over";
+		gameOver = true;
+	}
+}
